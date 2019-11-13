886abc9a64d43e505b1939ac5a90ac7655f17fc3
Jiri Cizek <jiri.cizek@broadcom.com>
2019-11-13 14:18:44 +0100

    Added integration with OpenWeatherMap

--- Integration with Wikipedia and OpenWeatherMap/WebHook.js
+++ Integration with Wikipedia and OpenWeatherMap/WebHook.js
@@ -15,7 +15,7 @@
  
  function main(params) {
     
-    const options = {
+   if (params.action == "Wiki") { const options = {
      uri: encodeURI("https://cs.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro&explaintext&titles=" + params.titles),
      json : true
      }
@@ -23,15 +23,13 @@
     return rp(options)
          .then (function (res) {
 
-            console.log('then');
-                 
              var index = '';
              for (var key in res['query']['pages']) {
                 index = key
                 }
             if (res['query']['pages'][index]['extract'] == "" || index == "-1" ) {
 
-                res['query']['pages'][index]['extract'] = "Bohužel, na Wiki není o tomhle žádné info."
+                res['query']['pages'][index]['extract'] = "Tak bohužel, na české Wiki není o "+ params.titles + " žádné info."
              }
              return { response : res['query']['pages'][index]['extract'] }
          })
@@ -39,6 +37,26 @@
             console.log('catch');
             console.log(err)
          })
+   } else if (params.action == "Weather") {
+        const options = {
+            uri: encodeURI("http://api.openweathermap.org/data/2.5/weather?apikey=4a2360d14bf33378079d2e2d49e35ddb&mode=json&units=metric&q=" + params.titles),
+            json : true
+        }
+       return rp(options)
+          .then (function (res) {
+
+           if (res['cod'] != "200" ) {
+                res['main'['temp']] = "Tak bohužel, město "+ params.titles + "  jsem nenašel na mapě."
+             }
+             return { response : res }
+         })
+         .catch (function (err) {
+            console.log('catch');
+            console.log(err)
+         }) 
+      
+   }
+   
  }
 
 //main(params)
